c14599220721a47d9a2dc9bc6c1cedf6dc37a5d0
Ben Newell <bennewell.mail@gmail.com>
2020-09-26 18:47:44 -0500

    [add]

--- new.py
+++ new.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python3
-# Compute pi at any point in series
-#def computer()
-#    list1 = [1, 101, 201, 301, 401, 501, 601, 701, 801, 901]
-#    pi = 0
-#    change = 4 / ((i * 2) * ((i * 2) + 1) * ((i * 2) + 2))
 
 def m(i):
     # define variables
@@ -11,6 +6,8 @@ def m(i):
     operator = 1
     denominator = 1
     run = 0
+    list1 = [1, 101, 201, 301, 401, 501, 601, 701, 801, 901]
+    answ = []
     while run < i:
         pi = pi + (4 * operator * (1 / denominator))
         # incrases the denominator by 2 each run.
@@ -21,5 +18,9 @@ def m(i):
         else:
             operator = -1
         run += 1
-        print(f"{run}        {round(pi, 4)}")
-m(901) 
+        #print(f"{run}        {format(pi, ".4f")}")
+        answ.append((run, format(pi, ".4f")))
+    for item in list1:
+        # item - 1 because lists start at 0 in python instead of 1
+        print(f"{answ[item - 1][0]}            {answ[item - 1][1]}")
+m(901)
